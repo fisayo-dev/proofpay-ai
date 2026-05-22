2af620f48771b7c604c0204c692b08b86b701caa
fisayo-dev <olufisayobadina@gmail.com>
2026-05-22 13:59:20 +0100

    feat: ship very minimalistic footer

--- web/layout/home_layout.tsx
+++ web/layout/home_layout.tsx
@@ -1,10 +1,12 @@
 import { SiteHeader } from "@/components/home/site-header";
+import { SiteFooter } from "@/components/home/site-footer";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <SiteHeader />
-      <div className="py-32 app-container">{children}</div>
+      <main className="app-container py-32">{children}</main>
+      <SiteFooter />
     </div>
   );
 };
